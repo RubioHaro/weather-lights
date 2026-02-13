386a77530dd57fa989b853f9db0d0c161b150961
Rodrigo (Roy) <chavo0022009@gmail.com>
2026-02-13 14:19:25 -0600

    web: {chore: tailwind and maps deps were added }

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        palmetto: {
-          green: '#2ecc71',
-          yellow: '#f1c40f',
-          red: '#e74c3c',
-          dark: '#1a1a1a',
-          primary: '#F9593B'
-        }
+        green: '#2ecc71',
+        yellow: '#f1c40f',
+        red: '#e74c3c',
+        dark: '#1a1a1a',
+        primary: '#F9593B'
+
       }
     },
   },
